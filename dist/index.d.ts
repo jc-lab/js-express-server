d0f88718f9bb9525d796e342aa3c70814746e738
Jichan <development@jc-lab.net>
2020-04-28 13:58:24 +0900

    remove dist

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import * as RdbService from './server-modules/rdb-service';
-import * as MessageResolver from './server-modules/message-resolver';
-import * as CloudKmsService from './server-modules/cloud-kms-service';
-import * as CloudStorageService from './server-modules/cloud-storage-service';
-export * from './server';
-export * from './server-module';
-export * from './server-modules/factory';
-export { RdbService, MessageResolver, CloudKmsService, CloudStorageService };
-//# sourceMappingURL=index.d.ts.map
